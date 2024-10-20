3990868ec72f9934fab12523ee7344390f1f9b71
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2024-10-19 20:04:38 -0400

    run two  test case

--- playwright-automation/tests/day01/paywrightIntro.spec.js
+++ playwright-automation/tests/day01/paywrightIntro.spec.js
@@ -1,6 +1,12 @@
 // const { test, expect } = require("@playwright/test");
 import { test } from "@playwright/test"; // import the test function from @playwright/test
 
-test("Simple Playwrite Automation Test", async ({ page }) => {
+test("Simple Playwrite Automation Test @google", async ({ page }) => {
   await page.goto("https://www.google.com/");
+  await page.waitForTimeout(3000);
+});
+
+test("Simple Playwrite Automation Test @youtube", async ({ page }) => {
+  await page.goto("https://www.youtube.com/");
+  await page.waitForTimeout(3000);
 });
