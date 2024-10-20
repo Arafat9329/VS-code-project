9a5ae848247b23c028e3a0585da3e68dd24e4c20
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2024-10-19 20:20:49 -0400

    first end to end test

--- playwright-automation/tests/day01/paywrightIntro.spec.js
+++ playwright-automation/tests/day01/paywrightIntro.spec.js
@@ -4,6 +4,19 @@ import { test } from "@playwright/test"; // import the test function from @playw
 test("Simple Playwrite Automation Test @google", async ({ page }) => {
   await page.goto("https://www.google.com/");
   await page.waitForTimeout(3000);
+
+  /*
+  <textarea class="gLFyf" aria-controls="Alh6id" aria-owns="Alh6id" autofocus="" title="Search" value="" jsaction="paste:puy29d;" aria-label="Search" aria-autocomplete="both" aria-expanded="false" aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" id="APjFqb" maxlength="2048" name="q" role="combobox" rows="1" spellcheck="false" data-ved="0ahUKEwjjgaOC1ZuJAxURIjQIHc9dD-AQ39UDCAc"></textarea>
+*/
+  //textarea[@class="gLFyf"];
+
+  //found by AI: '//textarea[@id="APjFqb"]'
+  let searchBox = page.locator('//textarea[@id="APjFqb"]');
+  searchBox.fill("playwright automation test");
+  await page.waitForTimeout(3000);
+
+  searchBox.press("Enter");
+  await page.waitForTimeout(3000);
 });
 
 test("Simple Playwrite Automation Test @youtube", async ({ page }) => {
