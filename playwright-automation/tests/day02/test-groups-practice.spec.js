bc382332edc0638d86201b4067e0fcaa3ca421ad
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2024-11-21 18:33:15 -0500

    day02

--- playwright-automation/tests/day02/test-groups-practice.spec.js
+++ playwright-automation/tests/day02/test-groups-practice.spec.js
@@ -1,24 +1,30 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Test Group @group2", () => {
-  test("Get the title of the page", async ({ page }) => {
+  //creare beforeach hook that will navigate to https://practice.cydeo.com/
+  test.beforeEach(async ({ page }) => {
     await page.goto("https://practice.cydeo.com/");
+  });
+
+  //create after each hook that will wait for 3 seconds before executing the test
+  test.afterEach(async ({ page }) => {
+    await page.waitForTimeout(3000);
+  });
+
+  test("Get the title of the page", async ({ page }) => {
     let actulTitle = await page.title();
     let expectedTitle = "Practice";
-
     expect(actulTitle).toEqual(expectedTitle);
   });
 
   test("Get the URL of the page", async ({ page }) => {
-    await page.goto("https://practice.cydeo.com/");
     let actulUrl = page.url();
-
     expect(actulUrl).toContain("cydeo");
   });
 
-  test("Click the A/B Testing link", async ({ page }) => {
-    await page.goto("https://practice.cydeo.com/");
-    let abTestingLink = page.locator("text=A/B Testing");
+  test("Click the A/B Testing link @group2-3", async ({ page }) => {
+    let abTestingLink = page.locator("a[href='/abtest']");
+    // let abTestingLink = page.locator("text=A/B Testing");
     // page.getByText("A/B Testing").click();
     expect(await abTestingLink.isEnabled()).toBeTruthy();
     await expect(await abTestingLink).toBeEnabled();
